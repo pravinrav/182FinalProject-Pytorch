58bb4c8908083b6ee48c0ba312bca71e6f8a4447
Pravin Ravishanker <pravinravishanker@Pravins-MacBook-Pro-2.local>
2020-05-08 00:49:11 -0700

    changes

--- model.py
+++ model.py
@@ -1,5 +1,19 @@
+from __future__ import print_function, division
+
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
 import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+plt.ion()   # interactive mode
+
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 from torch import nn
@@ -7,12 +21,20 @@ from torch import nn
 
 class Net(nn.Module):
     def __init__(self, num_classes, im_height, im_width):
+
         super(Net, self).__init__()
-        self.layer1 = nn.Linear(im_height * im_width * 3, 128)
-        self.layer2 = nn.Linear(128, num_classes)
+        self.resnet = models.resnet18(pretrained = True)
+
+        numFeatures = self.reset.fc.in_features
+
+        self.layer2 = nn.Linear(numFeatures, len(num_classes))
 
     def forward(self, x):
         x = x.flatten(1)
-        x = F.relu(self.layer1(x))
+
+        x = self.resnet(x)
         x = self.layer2(x)
+
+        x = torch.nn.softmax(x)
+        
         return x
--- train_sample_torch.py
+++ train_sample_torch.py
@@ -14,8 +14,11 @@ from model import Net
 
 from torch import nn
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def main():
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs = 25):
+
     # Create a pytorch dataset
     data_dir = pathlib.Path('./data/tiny-imagenet-200')
     image_count = len(list(data_dir.glob('**/*.JPEG')))
@@ -34,13 +37,115 @@ def main():
     ])
     train_set = torchvision.datasets.ImageFolder(data_dir / 'train', data_transforms)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
-                                               shuffle=True, num_workers=4, pin_memory=True)
+                                               shuffle=True, num_workers = 4, pin_memory = True)
 
-    # Create a simple model
+
+    # Create the validation data generator
+    validation_set = torchvision.datasets.ImageFolder(data_dir / 'val/data', data_transforms)
+    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size = batch_size,
+                                               shuffle = True, num_workers = 4, pin_memory = True)
+
+
+    # Begin Time
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            if phase == 'train':
+                loader = train_loader
+            else if phase == 'val':
+                loader = validation_loader
+
+            for idx, (inputs, targets) in enumerate(loader):
+
+                inputs = inputs.to(device)
+                targets = targets.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+
+                    outputs = model(inputs)
+                    loss = criterion(outputs, targets)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    # Save best model weights
+    torch.save({'net': model.state_dict(), }, 'latest.pt')
+
+    return model
+
+
+def main():
+    # Create a simple model, with optimizer and loss criterion and learning rate scheduler
     model = Net(len(CLASS_NAMES), im_height, im_width)
-    optim = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
+
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+    # Make sure the model is on the GPU
+    model = model.to(device)
+
+    # Train the Model
+    fittedModel = train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs = 25)
+
+
+    '''
     for i in range(num_epochs):
+
         train_total, train_correct = 0,0
         for idx, (inputs, targets) in enumerate(train_loader):
             optim.zero_grad()
@@ -56,6 +161,7 @@ def main():
         torch.save({
             'net': model.state_dict(),
         }, 'latest.pt')
+    '''
 
 
 if __name__ == '__main__':
